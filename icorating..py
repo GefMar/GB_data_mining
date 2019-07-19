c18ddefd927074cb9f38b1e8f7ec5f65db3faf24
Kirill <36851185+Valdert-13@users.noreply.github.com>
2019-07-19 14:59:36 +0300

    +

--- icorating..py
+++ icorating..py
@@ -2,18 +2,35 @@ import requests
 import random
 import time
 import os
+import json
 
 try:
     os.mkdir('data_icorating')
 except:
     pass
 sate_url = 'https://icorating.com/ico/all/load/'
-params = {'page':1}
-sate_data = requests.get(sate_url, params=params)
-while params["page"]<177:
-    sate_data = requests.get(sate_url, params=params)
-    with open(f'data_icorating/icorating_page_{params["page"]}.html', 'w') as file:
-        file.write(sate_data.text)
-    params["page"] += 1
-    time.sleep(random.randint(1, 5))
-    print (params["page"]) 
+
+
+
+class icorating:
+    params = {'page':1}
+
+    def __init__(self, url):        
+        while True:
+            data = self.sate_data(url, self.params)
+            last_page = data.get('icos')['last_page']
+            with open(f'data_icorating/icorating_page_{self.params["page"]}.json', 'w') as file:
+                json.dump(data, file)
+            data_dict = data.get('icos')
+            if self.params['page'] == last_page:
+                break
+            self.params['page'] +=1
+
+
+
+    def sate_data(self, url, params):
+        return requests.get(sate_url, params=params).json()
+        
+if __name__ == '__main__':
+    conect = icorating(sate_url)
+    print("Finish")
