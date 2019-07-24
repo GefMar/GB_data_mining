85123cec4a49a508d0a67088a7fcaa9e97da6190
Spouk <spoukster@gmail.com>
2019-07-24 17:54:49 +0300

    Доделал первое задание 3го урока

--- icorating_save_to_Mongo.py
+++ icorating_save_to_Mongo.py
@@ -1,5 +1,7 @@
 import requests
 from pymongo import MongoClient
+from alchemy_orm import IcoRatingStartup
+from alchemy_orm import Base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import session
@@ -9,28 +11,48 @@ CLIENT = MongoClient('localhost', 27017)
 # Выбираем базу
 MONGO_DB = CLIENT.icorating_db
 # Создаем коллекцию
-COLLECTION = MONGO_DB.ico_startup
+COLLECTION = MONGO_DB.icos
 
 engine = create_engine('sqlite:///icorating.db')
+Base.metadata.create_all(engine)
+
+db_session = sessionmaker(bind=engine)
+db_session.configure(bind=engine)
 
 # Инициируем ссылку
-site_url = 'https://icorating.com/ico/all/load/'
-# Определяем параметры. Сортировка нужна для того чтобы одни и теже проекты не попадали на разные
-# страницы и не перезатирались
-params = {'sort': 'name', 'direction': 'asc', 'page': 1}
-# Получаем первую ссылку для получения количества страниц
-site_data = requests.get(site_url, params=params)
-site_data_json = site_data.json()
-# Узнаем общее количество страниц
-last_page = site_data_json['icos']['last_page']
+site_url = 'https://icorating.com/ico/all/load/?sort=name&direction=asc&page={}'
 
-# Запускаем цикл с перебором страниц
-for page in range(1, last_page + 1):
-    # Меняем номер страницы
-    params['page'] = page
-    site_data = requests.get(site_url, params=params)
-    site_data_json = site_data.json()['icos']['data']
-    # Перебираем все элементы в списке проектов на текущей странице
-    for startup in site_data_json:
-        # Сохраняем в базу
-        COLLECTION.insert_one(startup)
+#
+class ICORating:
+    icos = []
+    current_page = 1
+
+    def __init__(self, url):
+
+        last_page = self.get_data(url.format(self.current_page))['icos']['last_page']
+
+        while True:
+            if self.current_page > last_page:
+                break
+            else:
+                data = self.get_data(url.format(self.current_page))
+
+            for ico in data['icos']['data']:
+                self.icos.append(IcoRatingStartup(**ico))
+
+            self.current_page += 1
+
+        COLLECTION.insert_many(self.icos)
+        session = db_session()
+        session.add_all(self.icos)
+        session.commit()
+        session.close()
+
+
+    def get_data(self, url):
+        return requests.get(url).json()
+
+
+if __name__ == '__main__':
+    collection = ICORating(site_url)
+    print('Done')
